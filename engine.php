844cc2a0eb1ded7bc745ca5ea1b56ea39b29cc11
Aryan Ra <contact@aryanraj.com>
2019-04-23 12:26:43 +0530

    Create engine.php

---
+++ engine.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Customizations
+ * Plugin URI:  https://github.com/rockingaryan/customizations/
+ * Description: Contains site-specific custom code.
+ * Author:      Aryan Raj
+ * Author URI:  https://www.aryanraj.com/
+ * Version:     1.0.0
+ * License:     GPL-2.0+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package Customizations
+ */
